c9974af549aa5d6b296817f9094b8396bd46b3b3
Lucas Augusto <luckaugusto@gmail.com>
2016-05-08 00:39:23 -0300

    De novo

---
+++ src/model/Atleta.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Atleta {
+	private int id;
+	private String nome;
+	private char sexo;
+	private String pais;
+
+	public Atleta() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+
+
+}
---
+++ src/model/TesteMB.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class TesteMB implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Atleta atleta = new Atleta();
+	private List<Atleta> lista_atl = new ArrayList<>();
+	
+	public String teste() {
+		System.out.println("Hello");
+		return "Ola";
+	}
+
+	public Atleta getAtleta() {
+		return atleta;
+	}
+
+	public void setAtleta(Atleta atleta) {
+		this.atleta = atleta;
+	}
+
+	public List<Atleta> getLista_atl() {
+		return lista_atl;
+	}
+
+	public void setLista_atl(List<Atleta> lista_atl) {
+		this.lista_atl = lista_atl;
+	}
+	
+	public void adicionar(Atleta a){
+		lista_atl.add(a);
+		System.out.println(a.getNome());
+	}
+}
